799fc16c88810a62da6257c54439f691b30eff51
EconLab AI <econlabai@EconLabAI.fritz.box>
2024-08-27 00:03:48 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from langchain_openai import AzureOpenAI
 
 app = Flask(__name__)
 
+# Umgebungsvariablen für Azure OpenAI
+api_key = os.environ.get('API_KEY')
+api_version = os.environ.get('API_VERSION')
+azure_deployment = os.environ.get('AZURE_DEPLOYMENT')
+model_name = os.environ.get('MODEL_NAME')
 
 @app.route('/')
 def index():
@@ -18,15 +22,24 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+   req = request.form.get('req')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+   # Azure OpenAI Client
+   llm = AzureOpenAI(
+       api_key=api_key,
+       api_version=api_version,
+       azure_deployment=azure_deployment,
+       model_name=model_name,
+   )
+
+   response = llm.invoke(req)
+   
+   if req:
+       print('Request for hello page received with req=%s' % req)
+       return render_template('hello.html', req=response)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
+       print('Request for hello page received with no req or blank req -- redirecting')
        return redirect(url_for('index'))
 
-
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', port=8000)
